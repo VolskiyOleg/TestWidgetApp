a8c53a7cdb66cc9be713dd2e4d39da509ede3c93
Volskyi Oleh <oleg110996@gmail.com>
2019-08-06 15:00:42 +0300

    init

---
+++ app/src/main/java/com/volskyioleh/testwigettask/NewAppWidget.java
@@ -0,0 +1,54 @@
+package com.volskyioleh.testwigettask;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.os.Handler;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.os.Looper.getMainLooper;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(final Context context, final AppWidgetManager appWidgetManager,
+                                final int appWidgetId) {
+
+
+        // Construct the RemoteViews object
+        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        final Handler someHandler = new Handler(getMainLooper());
+        someHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                final CharSequence widgetText =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(new Date());
+                views.setTextViewText(R.id.appwidget_text, widgetText);
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+                someHandler.postDelayed(this, 1000);
+
+            }
+        }, 10);
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
